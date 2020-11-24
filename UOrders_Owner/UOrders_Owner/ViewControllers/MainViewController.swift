5336e87b63e9a6f84835ee62fc52743745d25ca0
sunnyangee <hwagae2003@gmail.com>
2020-11-24 19:44:55 +0900

    [OrderManage] 주문관리, 매장관리 탭 이동 재 개발

--- UOrders_Owner/UOrders_Owner/ViewControllers/MainViewController.swift
+++ UOrders_Owner/UOrders_Owner/ViewControllers/MainViewController.swift
@@ -8,11 +8,46 @@
 import UIKit
 
 class MainViewController: UIViewController {
-
-    @IBOutlet weak var navigationBar: UINavigationBar!
+    
+    @IBOutlet weak var myPageButton: UIImageView!
+    
+    @IBOutlet weak var cafeNameLabel: UILabel!
+    @IBOutlet weak var cafeLoactionLabel: UILabel!
+    
+    @IBOutlet weak var orderManageButton: UIView!
+    @IBOutlet weak var storeManageButton: UIView!
+    
+    @IBOutlet weak var orderManageHighlight: UIView!
+    @IBOutlet weak var storeManageHighlight: UIView!
+    
+    @IBOutlet weak var orderManageView: UIView!
+    @IBOutlet weak var storeManageView: UIView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        orderManageView.isHidden = false
+        storeManageView.isHidden = true
+        
+        orderManageHighlight.backgroundColor = UIColor.orange
+        storeManageHighlight.backgroundColor = UIColor.gray
+    }
+    
+    @IBAction func orderManageTap(_ sender : UIGestureRecognizer){
+        print("order tap touch")
+        orderManageView.isHidden = false
+        storeManageView.isHidden = true
+        orderManageHighlight.backgroundColor = UIColor.orange
+        storeManageHighlight.backgroundColor = UIColor.gray
+    }
+    
+    @IBAction func storeManageTap(_ sender : UIGestureRecognizer){
+        print("store tap touch")
+        orderManageView.isHidden = true
+        storeManageView.isHidden = false
+        orderManageHighlight.backgroundColor = UIColor.gray
+        storeManageHighlight.backgroundColor = UIColor.orange
     }
     
 
--- UOrders_Owner/UOrders_Owner/ViewControllers/OrderManageVC.swift
+++ UOrders_Owner/UOrders_Owner/ViewControllers/OrderManageVC.swift
@@ -8,20 +8,13 @@
 import UIKit
 
 class OrderManageVC: UITableViewController {
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1;
+        return 3;
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
