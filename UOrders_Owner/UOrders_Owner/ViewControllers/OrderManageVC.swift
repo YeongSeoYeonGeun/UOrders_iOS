de51d3b6dfb622b3f74090f7be83668fff8c1cee
sunnyangee <hwagae2003@gmail.com>
2020-11-30 15:36:38 +0900

    [OrderManage] 주문 관리 전체 테이블 -> collectionview 수정

--- UOrders_Owner/UOrders_Owner/ViewControllers/OrderManageVC.swift
+++ UOrders_Owner/UOrders_Owner/ViewControllers/OrderManageVC.swift
@@ -19,9 +19,11 @@ class OrderManageVC: UIViewController {
                      Order(customerName : "시연 님", orderNumber: "(No. 17)", leftTime: "13 분"),
                      Order(customerName : "시연 님", orderNumber: "(No. 17)", leftTime: "13 분"),
                      Order(customerName : "시연 님", orderNumber: "(No. 17)", leftTime: "13 분"),
-                     Order(customerName : "시연 님", orderNumber: "(No. 17)", leftTime: "13 분"),]
+                     Order(customerName : "시연 님", orderNumber: "(No. 17)", leftTime: "13 분")
+                     ]
     
-    @IBOutlet weak var orderTableView: UITableView!
+    
+    @IBOutlet weak var orderCollectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,32 +31,71 @@ class OrderManageVC: UIViewController {
     }
     
     func setOrderTableView() {
-        orderTableView.delegate = self
-        orderTableView.dataSource = self
-        orderTableView.tableFooterView = UIView()
-        orderTableView.estimatedRowHeight =  185
-        orderTableView.rowHeight = UITableView.automaticDimension
+        orderCollectionView.delegate = self
+        orderCollectionView.dataSource = self
+        
+        let orderLayout = UICollectionViewFlowLayout()
+        orderLayout.minimumLineSpacing = 0
+        orderLayout.minimumInteritemSpacing = 0
+        orderLayout.sectionInset = UIEdgeInsets.zero
+        
+        let collectionWidth = UIScreen.main.bounds.width
+        orderLayout.itemSize = CGSize(width : collectionWidth, height: collectionWidth * 0.7)
+        self.orderCollectionView.collectionViewLayout = orderLayout
+        
+        
     }
 
 }
 
-extension OrderManageVC : UITableViewDelegate, UITableViewDataSource {
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return orderList.count
+extension OrderManageVC : UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "orderTableViewCell", for: indexPath) as! OrderTableViewCell
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
-        cell.customerNameLabel.text = orderList[indexPath.row].customerName
-        cell.orderNumberLabel.text = orderList[indexPath.row].orderNumber
-        cell.leftTimeLabel.text = orderList[indexPath.row].leftTime
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "orderCollectionViewCell", for: indexPath)
         
         return cell
     }
-
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 200
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return self.orderList.count
     }
+    
+    
 }
+
+
+//class HomeViewController: TaskManageViewController {
+//    private var recommendedProductSizingCell: UIView!
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        recommendedProductSizingCell = Bundle.main.loadNibNamed(String(describing: RecommendedProductCollectionViewCell.self), owner: self, options: nil)?.first as? UIView
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView,
+//                        layout collectionViewLayout: UICollectionViewLayout,
+//                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+//        switch collectionView {
+//        case personalizedRecommendationCollectionView:
+//            let layout = personalizedRecommendationCollectionView.collectionViewLayout as! UICollectionViewFlowLayout
+//            let sectionInset = layout.sectionInset
+//            var margin: CGFloat = sectionInset.left + sectionInset.right
+//            let numberOfItems = self.collectionView(personalizedRecommendationCollectionView, numberOfItemsInSection: indexPath.section)
+//            let minimumLineSpacing = self.collectionView(collectionView, layout: layout, minimumLineSpacingForSectionAt: indexPath.section)
+//            margin += CGFloat(numberOfItems - 1) * minimumLineSpacing
+//            let width: CGFloat = (view.frame.width - margin) / CGFloat(numberOfItems)
+//
+//            recommendedProductSizingCell.imageViewWidthConstraint.constant = width
+//
+//            let fittingSize =  recommendedProductSizingCell.systemLayoutSizeFitting(UIView.layoutFittingCompressedSize)
+//
+//            maxRecommendedProductCellHeight = max(maxRecommendedProductCellHeight, fittingSize.height + sectionInset.top + sectionInset.bottom)
+//            personalizedRecommendationCollectionViewHeightConstraint.constant = maxRecommendedProductCellHeight
+//
+//            return CGSize(width: width, height: fittingSize.height)
+//    }
+//}
---
+++ UOrders_Owner/UOrders_Owner/Views/OrderCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  OrderCollectionViewCell.swift
+//  UOrders_Owner
+//
+//  Created by SEONYOUNG LEE on 2020/11/30.
+//
+
+import UIKit
+
+class OrderCollectionViewCell: UICollectionViewCell {
+    
+}
