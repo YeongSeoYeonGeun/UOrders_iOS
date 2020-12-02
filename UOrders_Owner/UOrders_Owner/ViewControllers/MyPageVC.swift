ff91b03c788eac64b850661444f2117ef72e0cf8
ssionii <ssionii353@gmail.com>
2020-12-02 22:58:49 +0900

    [mypage] 위쪽 화면 구성 완료

---
+++ UOrders_Owner/UOrders_Owner/ViewControllers/MyPageVC.swift
@@ -0,0 +1,56 @@
+//
+//  MyPageVC.swift
+//  UOrders_Owner
+//
+//  Created by 양시연 on 2020/12/02.
+//
+
+import UIKit
+
+class MyPageVC: UIViewController {
+    
+    @IBOutlet weak var cafeImageBorderView: UIView!
+    @IBOutlet weak var cafeImageView: UIImageView!
+    @IBOutlet weak var cafeNameLabel: UILabel!
+    @IBOutlet weak var cafeLocationLabel: UILabel!
+    
+    @IBOutlet weak var profitCheckView: UIView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setView()
+    }
+    
+    func setView(){
+        // profile 동그랗게
+        
+        cafeImageBorderView.layer.cornerRadius =        cafeImageView.frame.height/2
+        cafeImageView.layer.cornerRadius =        cafeImageView.frame.height/2
+        cafeImageView.layer.borderWidth = 1
+        cafeImageView.layer.borderColor = UIColor.clear.cgColor
+        cafeImageView.clipsToBounds = true
+        
+        // 수익내역 radius, 그림자
+        profitCheckView.layer.cornerRadius = 20
+        profitCheckView.layer.shadowColor = UIColor.black.cgColor
+        profitCheckView.layer.masksToBounds = false
+        profitCheckView.layer.shadowOffset = CGSize(width: 0, height: 4)
+        profitCheckView.layer.shadowRadius = 8
+        profitCheckView.layer.shadowOpacity = 0.16
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
