a6f8b556d639013b1d9fcb9c6aa5f11aacff7c5a
sunnyangee <hwagae2003@gmail.com>
2020-12-02 13:21:37 +0900

    [OrderManage] 주문 도착 화면 완료

---
+++ UOrders_Owner/UOrders_Owner/Views/ArrivedItemTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ArrivedItemTableViewCell.swift
+//  UOrders_Owner
+//
+//  Created by SEONYOUNG LEE on 2020/12/02.
+//
+
+import UIKit
+
+class ArrivedItemTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
