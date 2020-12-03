b8892ad720ba18f9346ebb59714d937e1e0d1a00
sunnyangee <hwagae2003@gmail.com>
2020-12-03 21:53:25 +0900

    [OrderManage] tableview header 데이터 적용, autolayout 적용 완료

--- UOrders_Owner/UOrders_Owner/SupportingFiles/OrderListSectionHeader.swift
+++ UOrders_Owner/UOrders_Owner/SupportingFiles/OrderListSectionHeader.swift
@@ -19,9 +19,9 @@ class OrderListSectionHeader: UITableViewHeaderFooterView {
     
     let customerNameLabel = UILabel()
     let orderNumberLabel = UILabel()
+    let untilCompleteLabel = UILabel()
     let leftTimeLabel = UILabel()
-    
-    
+
     override init(reuseIdentifier: String?) {
         super.init(reuseIdentifier: reuseIdentifier)
        
@@ -33,18 +33,52 @@ class OrderListSectionHeader: UITableViewHeaderFooterView {
     
     override func awakeFromNib() {
        super.awakeFromNib()
-       //custom logic goes here
-        configureContent()
+        self.contentView.backgroundColor = UIColor.white
+        
+        customerNameLabel.text = "승아 님"
+        customerNameLabel.font = customerNameLabel.font.withSize(20)
+        customerNameLabel.textColor = UIColor(red: 68/255, green: 68/255, blue: 68/255, alpha: 1)
+        
+        orderNumberLabel.text = "(No. 32)"
+        orderNumberLabel.font = orderNumberLabel.font.withSize(20)
+        orderNumberLabel.textColor = UIColor(red: 68/255, green: 68/255, blue: 68/255, alpha: 1)
+        
+        untilCompleteLabel.text = "완료까지"
+        untilCompleteLabel.font = untilCompleteLabel.font.withSize(20)
+        untilCompleteLabel.textColor = UIColor(red: 68/255, green: 68/255, blue: 68/255, alpha: 1)
+        
+        leftTimeLabel.text = "25분"
+        leftTimeLabel.font = UIFont.boldSystemFont(ofSize: 20)
+        leftTimeLabel.textColor = UIColor(red: 11/255, green: 70/255, blue: 60/255, alpha: 1)
+        
+        setContentOnView()
     }
     
-    func configureContent() {
+    func setContentOnView() {
         customerNameLabel.translatesAutoresizingMaskIntoConstraints = false
         orderNumberLabel.translatesAutoresizingMaskIntoConstraints = false
+        untilCompleteLabel.translatesAutoresizingMaskIntoConstraints = false
         leftTimeLabel.translatesAutoresizingMaskIntoConstraints = false
-
-        contentView.addSubview(customerNameLabel)
-        contentView.addSubview(orderNumberLabel)
-        contentView.addSubview(leftTimeLabel)
+        
+        
+        self.contentView.addSubview(customerNameLabel)
+        self.contentView.addSubview(orderNumberLabel)
+        self.contentView.addSubview(untilCompleteLabel)
+        self.contentView.addSubview(leftTimeLabel)
+        
+        //content autolayout 적용
+        customerNameLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor).isActive = true
+        customerNameLabel.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: 20).isActive = true
+        
+        orderNumberLabel.centerYAnchor.constraint(equalTo: customerNameLabel.centerYAnchor).isActive = true
+        orderNumberLabel.leftAnchor.constraint(equalTo: customerNameLabel.rightAnchor, constant: 5).isActive = true
+        
+        leftTimeLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor).isActive = true
+        leftTimeLabel.rightAnchor.constraint(equalTo: contentView.rightAnchor, constant: -20).isActive = true
+        
+        untilCompleteLabel.centerYAnchor.constraint(equalTo: leftTimeLabel.centerYAnchor).isActive = true
+        untilCompleteLabel.rightAnchor.constraint(equalTo: leftTimeLabel.leftAnchor, constant: -5).isActive = true
+        
     }
     
     func stackViewFactory(type : NSLayoutConstraint.Axis) -> UIStackView {
